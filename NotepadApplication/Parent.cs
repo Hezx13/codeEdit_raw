0a4c893eab63fb70d10229c1912c18b093b86331
Hezx13 <skzalupa@gmail.com>
2023-05-18 01:56:12 +0200

    initial commit

---
+++ NotepadApplication/Parent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using EasyTabs;
+
+
+namespace NotepadApplication
+{
+    public partial class Parent : TitleBarTabs
+    {
+        public Parent()
+        {
+            InitializeComponent();
+
+            AeroPeekEnabled = true;
+            TabRenderer = new ChromeTabRenderer(this);
+        }
+
+        public override TitleBarTab CreateTab()
+        {
+            return new TitleBarTab(this)
+            {
+                Content = new Form1()
+                {
+                    Text = "New File"
+                }
+            };
+        }
+
+        private void Parent_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NotepadApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyTabs;
+
+namespace NotepadApplication
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Parent container = new Parent();
+
+            container.Tabs.Add(
+                new TitleBarTab(container)
+                {
+                    Content = new Form1()
+                    {
+                        Text = "New File"
+                    }
+                }
+                );
+            container.SelectedTabIndex = 0;
+
+            TitleBarTabsApplicationContext context = new TitleBarTabsApplicationContext();
+            context.Start(container);
+            Application.Run(context);
+
+        }
+    }
+}
